026a0797303e43b0f190a65b9d85e0cf12c96563
Killian <killian.kleine@etu.iut-tlse3.fr>
2020-03-03 11:48:52 +0100

    layout pour resoudre le bug

--- app/src/main/java/projet/projetapplication/NouveauJeu.java
+++ app/src/main/java/projet/projetapplication/NouveauJeu.java
@@ -10,5 +10,6 @@ public class NouveauJeu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nouveau_jeu);
+        setContentView(R.layout.);
     }
 }
