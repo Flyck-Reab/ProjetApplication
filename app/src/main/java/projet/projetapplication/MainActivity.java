5de299e4f02191f61c1fdc13ea437c8e6b8b338e
SirPepit0 <braudcorentin@hotmail.fr>
2020-03-03 20:45:23 +0100

    menu trois-points ajouté à l'interface

--- app/src/main/java/projet/projetapplication/MainActivity.java
+++ app/src/main/java/projet/projetapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package projet.projetapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view)
     {
         startActivity(new Intent(MainActivity.this,NouveauJeu.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_threedots, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menuAnglais:
+                break;
+            case R.id.menuFrancais:
+                break;
+        }
+        return true;
     }
 }
