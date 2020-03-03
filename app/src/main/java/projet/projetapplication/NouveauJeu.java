b0f07ba6aca5e8a1a89e8b349bce5a5e45d76b51
Killian <killian.kleine@etu.iut-tlse3.fr>
2020-03-03 11:51:42 +0100

    supression d'un layout, bug corrigé : erreur android studio

--- app/src/main/java/projet/projetapplication/NouveauJeu.java
+++ app/src/main/java/projet/projetapplication/NouveauJeu.java
@@ -10,6 +10,5 @@ public class NouveauJeu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nouveau_jeu);
-        setContentView(R.layout.);
     }
 }
