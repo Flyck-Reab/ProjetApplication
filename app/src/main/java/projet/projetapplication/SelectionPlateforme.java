c76ec6bee049bacb73bbd5cbe2c1e4ba81174874
Killian <killian.kleine@etu.iut-tlse3.fr>
2020-03-09 08:20:21 +0100

    Suite ajout jeu

---
+++ app/src/main/java/projet/projetapplication/SelectionPlateforme.java
@@ -0,0 +1,14 @@
+package projet.projetapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionPlateforme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_plateforme);
+    }
+}
