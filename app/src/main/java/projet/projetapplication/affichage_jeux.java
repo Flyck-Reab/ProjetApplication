4621b719820f2cc8303a96c817edfd6f05e52ffa
sirpepit0 <rewt2000@gmail.com>
2020-03-11 08:28:17 +0100

    La bdd marche (?)

--- app/src/main/java/projet/projetapplication/affichage_jeux.java
+++ app/src/main/java/projet/projetapplication/affichage_jeux.java
@@ -3,13 +3,21 @@ package projet.projetapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.sqliteoperations.myDbAdapter;
+
 class affichageJeux extends AppCompatActivity {
+
+    private myDbAdapter.myDbHelper myhelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affichage_jeux);
+        Intent i = new Intent(MainActivity.this, DisplayActivity.class);
     }
+
 }
